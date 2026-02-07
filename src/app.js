a33a3656a63f6f54be723c44251085c923d5364b
chirinosmiguel <miguelangel_ch@hotmail.com>
2026-02-07 00:58:01 +0000

    exercise1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Miguel from the console!");
+  let age = 24;
+  console.log(age);
+  console.log(typeof(age));
+
+  let MiNombreEs = "Miguel";
+  console.log(MiNombreEs);
+  let HijoMayor = {'Nombre':'Luca', 'Edad':7};
+  console.log(HijoMayor.Nombre);
+  let Arreglo1 =[1,'dos',3,true];
+  console.log(Arreglo1[1]);
 };
